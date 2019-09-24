074ef3beeb529337dc3be04b2c31c76ac7016105
jmlemoine <jeanmelvinlp27@gmail.com>
2019-09-24 16:33:51 -0400

    Haciendo que el programa pida la URL deseada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException{
+
+        //Se digita la URL
+        System.out.println("Escriba la URL de la página que desee: ");
+        Scanner url = new Scanner(System.in);
+        String pagina = url.next();
 
     }
 
