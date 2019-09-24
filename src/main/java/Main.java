74487b654e9395a919161089e67636bb874ddf8e
jmlemoine <jeanmelvinlp27@gmail.com>
2019-09-24 16:38:23 -0400

    Haciendo la conexión de la página y la librería Jsoup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -5,10 +8,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
 
-        //Se digita la URL
+        //Se digita la URL.
         System.out.println("Escriba la URL de la página que desee: ");
         Scanner url = new Scanner(System.in);
         String pagina = url.next();
+
+        //Conexión a la página digitada a través de la librería Jsoup, creando una conexión HTTP.
+        Connection.Response conexion = Jsoup.connect(pagina).execute();
 
     }
 
