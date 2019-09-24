63e0bad0465d7be1bf6076f9dc9f7948990419cc
jmlemoine <jeanmelvinlp27@gmail.com>
2019-09-24 16:55:47 -0400

    Indicando la cantidad de párrafos que contiene el documento HTML

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
+import org.jsoup.nodes.Document;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -21,7 +22,17 @@ public class Main {
         //length: Se muestra el tamaño del cuerpo después de la función split("\n") haber dividido el cuerpo por cada salto de línea.
         String cuerpo = conexion.body();
         int cantLineas = cuerpo.split("\n").length;
-        System.out.println("\na) La cantidad de líneas del recurso retornado es: "+cantLineas);
+        System.out.println("\na) La cantidad de líneas del recurso retornado es: "+cantLineas + "\n");
+
+        //b) Indicar la cantidad de párrafos <p> que contiene el documento HTML.
+        // get(): Obtiene el documento HTML de la URL introducida.
+        // connect(): Se conecta a la página al igual que como se utilizó para ejecutar una conexión con execute() anteriormente.
+        // getElementsByTag("p"): Obtiene todos los elementos que tengan una etiqueta <p>.
+        // size(): Se obtiene la cantidad de párrafos que hay en el arreglo de elementos.
+        Document docHTML = Jsoup.connect(pagina).get();
+        int cantParrafos = docHTML.getElementsByTag("p").size();
+        System.out.println("\nb) La cantidad de párrafos que contiene el documento HTML es: "+cantParrafos + "\n");
+
 
     }
 
